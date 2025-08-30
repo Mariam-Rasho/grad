e06de8c01be959e22e01272f871f6564cdb38440
mariam <rashomariam242@gmail.com>
2025-08-30 06:04:21 -0700

    edit file config

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,7 @@
 import type { NextConfig } from "next";
+
 const isProd = process.env.NODE_ENV === 'production';
+const repoName = 'Mariam-Rasho'; // Your repository name
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -12,6 +14,16 @@ const nextConfig: NextConfig = {
   // assetPrefix: isProd ? 'https://Mariam-Rasho' : '',
   // basePath: isProd ? '/Mariam-Rasho' : '',
   // output: 'export'
+
+    images: {
+    unoptimized: true,
+  },
+  // Remove or fix assetPrefix if it's causing issues
+  assetPrefix: isProd ? `/${repoName}/` : '',
+  basePath: isProd ? `/${repoName}` : '',
+  output: 'export',
+  // Add trailing slash for GitHub Pages compatibility
+  trailingSlash: true,
 };
 
 export default nextConfig;
