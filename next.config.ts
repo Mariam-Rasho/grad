92626ce9a7e5870c3e25fa784aee0d76e3531495
mariam <rashomariam242@gmail.com>
2025-08-29 23:16:00 -0700

    deployed website

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,17 @@
 import type { NextConfig } from "next";
+// const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+
+  // images: {
+  //   unoptimized: true, // Disable default image optimization
+  // },
+
+  // assetPrefix: isProd ? 'Mariam-Rasho' : '',
+  // basePath: isProd ? '/Mariam-Rasho' : '',
+  // output: 'export'
 };
 
 export default nextConfig;
